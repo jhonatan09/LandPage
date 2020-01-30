51d527bb6435be517c98120df25b08fe26cb8fbc
Jhonatan <jhonatancardoso09@gmail.com>
2020-01-30 13:57:36 -0300

    Updates

---
+++ precache-manifest.56bb103c902806dffb704efad8f46d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fce59da81a8ee6cdfee1e16fa44491",
+    "url": "/LandPage/index.html"
+  },
+  {
+    "revision": "4063afebd4698bcc1ede",
+    "url": "/LandPage/static/css/main.a6b3881f.chunk.css"
+  },
+  {
+    "revision": "6f1b3a2ce2539d01ed1e",
+    "url": "/LandPage/static/js/2.17ec4e87.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/LandPage/static/js/2.17ec4e87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4063afebd4698bcc1ede",
+    "url": "/LandPage/static/js/main.00da70d4.chunk.js"
+  },
+  {
+    "revision": "08942ac0015f7c18949f",
+    "url": "/LandPage/static/js/runtime-main.57533093.js"
+  }
+]);
